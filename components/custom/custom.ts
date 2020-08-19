ee20ec19285e374fe538396b804714e979f31ae0
kcs <2815443705@qq.com>
2020-08-19 10:46:33 +0800

    fix:更改配置

--- components/custom/custom.ts
+++ components/custom/custom.ts
@@ -1,21 +1,17 @@
-import CustomLogo from './custom-logo.vue';
-import CustomPerm from './custom-perm.vue';
-import CustomDict from './custom-dict.vue';
-import CustomInput from './custom-input.vue';
-import CustomDrawer from './custom-drawer.vue';
-import CustomFileUpload from './custom-file-upload.vue';
-import CustomAddressCity from './custom-address-city.vue';
-import CustomPaging from './custom-paging.vue';
-import CustomCollapse from './custom-collapse.vue';
-import CustomUpload from './custom-upload.vue';
-// import CustomTable from "./custom-table.vue";
-import CustomFloatNavigation from "./custom-float-navigation.vue";
-// import CustomForm from "./custom-form.vue";
-import CustomBtnList from "./custom-button-list.vue"
+import CustomLogo from './custom-logo.vue'
+import CustomPerm from './custom-perm.vue'
+import CustomDict from './custom-dict.vue'
+import CustomInput from './custom-input.vue'
+import CustomDrawer from './custom-drawer.vue'
+import CustomFileUpload from './custom-file-upload.vue'
+import CustomAddressCity from './custom-address-city.vue'
+import CustomPaging from './custom-paging.vue'
+import CustomCollapse from './custom-collapse.vue'
+import CustomUpload from './custom-upload.vue'
+import CustomFloatNavigation from './custom-float-navigation.vue'
+import CustomBtnList from './custom-button-list.vue'
 
 const components: any[] = [
-  // CustomForm,
-  // CustomTable,
   CustomBtnList,
   CustomLogo,
   CustomPerm,
@@ -28,16 +24,17 @@ const components: any[] = [
   CustomFileUpload,
   CustomAddressCity,
   CustomFloatNavigation
-];
+]
 
 const install = (Vue: any) => {
   components.map((value) => {
-    Vue.component(value.name, value);
-  });
-};
-
-/* istanbul ignore if */
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
+    Vue.component(value.name, value)
+  })
 }
-export default Object.assign(components, {install});
+
+declare const window: Window & any
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+export default Object.assign(components, {install})
